f29962e31b08bdb2718ce9a398e6808fd80ae79e
HammadNadeem21 <nhammad424@gmail.com>
2024-09-29 10:21:42 +0500

    updated

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
-// let menuIcon = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 // let sections =document.querySelectorAll('section');
 // let navLinks = document.querySelectorAll('header nav a');
 
@@ -24,37 +24,36 @@
 // }
 
 
-// menuIcon.onclick = () =>{
-//     menuIcon.classList.toggle('bx-x');
-//     navbar.classList.toggle('active');
-// }
-
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    let top = window.scrollY;
-
-    sections.forEach(sec => {
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href').includes(id)) {
-                    link.classList.add('active');
-                }
-            });
-        }
-    });
-};
-
-menuIcon.onclick = () => {
+menuIcon.onclick = () =>{
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-};
+}
+
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll = () => {
+//     let top = window.scrollY;
+
+//     sections.forEach(sec => {
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(link => {
+//                 link.classList.remove('active');
+//                 if (link.getAttribute('href').includes(id)) {
+//                     link.classList.add('active');
+//                 }
+//             });
+//         }
+//     });
+// };
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// };
